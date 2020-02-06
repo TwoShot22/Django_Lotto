603f10adefeeb4290639b1479e7d53faeac4d503
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-02-06 17:46:43 +0900

    Preparing Heroku

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$e9!#bk1)l2(4)^yu0e$zqo##tkp+(h1(3=&656qo=kt$1u+a*'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '$e9!#bk1)l2(4)^yu0e$zqo##tkp+(h1(3=&656qo=kt$1u+a*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
