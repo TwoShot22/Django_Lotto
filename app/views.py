6059d243492fd26b8fb1413b9a07e36b9b6c3bea
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-02-06 16:14:55 +0900

    Result Connect & Create Number Completed

--- app/views.py
+++ app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def result(request):
-    return render(request, 'result.html')
+    # User Input
+    number_list = list()
+
+    for i in range(6):
+        number = request.GET['number'+str(i+1)]
+        number_list.append(int(number))
+
+    # Random Output
+    random_list = list()
+    
+    for j in range(7):
+        number = random.randrange(1,46)
+        random_list.append(number)
+
+    return render(request, 'result.html', {'number_list':number_list, 'random_list':random_list})
