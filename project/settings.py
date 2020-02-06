cc693660f0089abf8eecfb003aa37f0466ba8c0e
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-02-06 17:48:38 +0900

    Preparing Heroku

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '$e9!#bk1)l2(4)^yu0e$zqo##tkp+(h1(3=&656qo=kt$1u+a*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
